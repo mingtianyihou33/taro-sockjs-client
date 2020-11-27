a758e4dca07db3b24ccfc5b1f48312a032889b71
mingtianyihou33 <liumingyi1428@126.com>
2020-11-27 17:11:43 +0800

    chore: debug

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -1,5 +1,6 @@
-export default function (...args) {
-  if (process.env.NODE_ENV !== 'production') {
-    console.debug(...args)
-  }
-}
+const debug = (function () {
+  return process.env.NODE_ENV !== 'production'
+    ? console.debug.bind(console)
+    : function () {}
+})()
+export default debug
